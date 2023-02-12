d8860fb0a220063edc89105280dcec7825166bcf
Lukasz Szmytka <szmytka983@gmail.com>
2023-02-12 17:06:58 +0100

    created array with currencies

---
+++ src/currencies.js
@@ -0,0 +1,8 @@
+export const currencies = [
+    {},
+    {id: "EUR", name: "Euro", rate: 4.697},
+    {id: "USD", name: "Dolar amerykański", rate: 4.401},
+    {id: "CHF", name: "Frank szwajcarski", rate: 4.7606},
+    {id: "TRY", name: "Lira turecka", rate: 0.2344},
+    {id: "GBP", name: "Funt brytyjski", rate: 5.3515},
+  ];
