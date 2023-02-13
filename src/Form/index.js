7892da0058b8438f58a7001ccaa957c396404a85
Lukasz Szmytka <szmytka983@gmail.com>
2023-02-13 20:25:01 +0100

    updated Form component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,21 +1,34 @@
 import "./style.css";
 import { useState } from "react";
 import { currencies } from "../currencies";
+import Result from "../Result";
 
 const Form = () => {
   const [amount, setAmount] = useState("");
-  const [currency, setCurrency] = useState("");
-  
+  const [currency, setCurrency] = useState(currencies[0].name);
+  const [result, setResult] = useState();
+  const [printResult, setPrintResult] = useState();
+  const rate = currencies.find(({ name }) => name === currency).rate;
+  const short = currencies.find(({name}) => name === currency).short;
+  const calculateResult = () => {
+    setResult(() => amount / rate);
+  };
+  const showResult = () => {
+    setPrintResult(() => ({ amount, short, rate }));
+  };
 
   const onFormSubmit = (event) => {
     event.preventDefault();
+    calculateResult();
+    showResult();
+    
   };
 
   return (
     <form className="form" onSubmit={onFormSubmit}>
       <h1 className="form__header">Kantor wymiany walut</h1>
       <p>
-        <label>
+        <label className="form__label">
           <span className="form__labelText">Podaj kwotę [PLN]* :</span>
           <input
             value={amount}
@@ -30,36 +43,28 @@ const Form = () => {
         </label>
       </p>
       <p>
-        <label>
-          <span className="form__labelText">Wybierz walutę :</span>
+        <label className="form__label">
+          <span className="form__labelText">Wybierz walutę* :</span>
           <select
             className="form__field"
             name="currency"
+            required
             value={currency}
             onChange={({ target }) => setCurrency(target.value)}
           >
-          {currencies.map((currency) => (
-            <option
-              key={currency.id}
-            >
-              {currency.name}
-            </option>
-          ))}
+            {currencies.map((currency) => (
+              <option key={currency.id}>{currency.name}</option>
+            ))}
           </select>
-        </label>
-      </p>
-      <p>
-        <label>
-          <span className="form__labelText">
-            Kurs z dnia 2023-01-09 [PLN] :
-          </span>
-          <strong className="form__field">{currency}</strong>
         </label>
       </p>
       <p>
         <button className="form__button">Przelicz</button>
       </p>
-      <p className="form__result"></p>
+      <Result
+        printResult={printResult}
+        result={result} 
+      />
     </form>
   );
 };
