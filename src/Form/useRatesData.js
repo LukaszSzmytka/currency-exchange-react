26580180ffd0c189bb9e275e1470df970f23183c
Lukasz Szmytka <szmytka983@gmail.com>
2023-10-20 21:06:35 +0200

    useRatesData hook edit

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -2,33 +2,31 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const useRatesData = () => {
-  const [ratesData, setRatesData] = useState({ status: "loading" });
+    const [ratesData, setRatesData] = useState({ status: "loading" });
 
-  useEffect(() => {
-    setTimeout(() => {
-      (async () => {
-        try {
-          // const response = await axios.get("https://api.exchangerate.host/latest?base=PLN")
-          const response = await axios.get(
-            "http://127.0.0.1:5500/src/Form/currenciesData.json"
-          );
-          console.log(response.data);
-          if (!response.ok) {
-            throw new Error(response.statusText);
-          }
-          const data = response.data;
-          setRatesData({
-            status: "succes",
-            data,
-          });
-        } catch {
-          setRatesData({
-            status: "error",
-          });
-        }
-      })();
-    }, 4000);
-  }, []);
+    useEffect(() => {
+        setTimeout(() => {
+          (async () => {
+            try {
+              // const response = await axios.get("https://api.exchangerate.host/latest?base=PLN")
+              const response = await axios.get("http://127.0.0.1:5500/src/Form/currenciesData.json");
+    
+              localStorage.setItem("data", JSON.stringify(response.data));
+    
+              setRatesData({
+                status: "success",
+              });
 
-  return ratesData;
+            } catch {
+              setRatesData({
+                status: "error",
+              });
+            }
+          })();
+        }, 3000);
+      }, []);
+    
+ 
+
+  return ratesData ;
 };
