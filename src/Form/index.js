77554d783b74f2eee3fa3c1a42d9675c8fe2726d
Lukasz Szmytka <szmytka983@gmail.com>
2023-10-31 16:04:04 +0100

    mooved const to useRatesData component

--- src/Form/index.js
+++ src/Form/index.js
@@ -4,10 +4,8 @@ import { Header, Label, LabelText, Input, Button } from "./styled";
 import { useRatesData } from "./useRatesData";
 
 const Form = () => {
-  const ratesData = useRatesData();
-  const dataFromLS = localStorage.getItem("data");
-  const currenciesData = JSON.parse(dataFromLS);
-  const currencies = Object.keys(currenciesData.rates);
+  const { ratesData, currencies, currenciesData, currenciesDate } =
+    useRatesData();
 
   const [amount, setAmount] = useState("");
   const [currency, setCurrency] = useState(currencies[0]);
@@ -16,9 +14,9 @@ const Form = () => {
 
   const rate = currenciesData.rates[currency];
 
-  function calculateResult() {
+  const calculateResult = () => {
     setResult(amount * rate);
-  }
+  };
 
   const showResult = () => {
     setPrintResult({ amount, currency, rate });
@@ -33,40 +31,59 @@ const Form = () => {
   return (
     <form onSubmit={onFormSubmit}>
       <Header>Kantor wymiany walut</Header>
-      <p>
-        <Label>
-          <LabelText>Podaj kwotę [PLN]* :</LabelText>
-          <Input
-            value={amount}
-            onChange={({ target }) => setAmount(target.value)}
-            name="amount"
-            type="number"
-            required
-            step="0.01"
-            min="0.01"
+      {ratesData.status === "loading" ? (
+        <p>
+          Sekundka...<br></br>Pobieram aktualne kursy walut z Europejskiego
+          Banku Centralnego...
+        </p>
+      ) : ratesData.status === "error" ? (
+        <p>
+          Hmm... Coś poszło nie tak. Sprawdź czy masz połączenie z internetem.
+          <br></br>Jeśli masz... to wygląda na to, że to nasza wina. Możesz
+          spróbować później?
+        </p>
+      ) : (
+        <>
+          <p>
+            <Label>
+              <LabelText>Podaj kwotę [PLN]* :</LabelText>
+              <Input
+                value={amount}
+                onChange={({ target }) => setAmount(target.value)}
+                name="amount"
+                type="number"
+                required
+                step="0.01"
+                min="0.01"
+              />
+            </Label>
+          </p>
+          <p>
+            <Label>
+              <LabelText>Wybierz walutę* :</LabelText>
+              <Input
+                as="select"
+                name="currency"
+                required
+                value={currency}
+                onChange={({ target }) => setCurrency(target.value)}
+              >
+                {currencies.map((currency) => (
+                  <option key={currency}>{currency}</option>
+                ))}
+              </Input>
+            </Label>
+          </p>
+          <p>
+            <Button>Przelicz</Button>
+          </p>
+          <Result
+            printResult={printResult}
+            result={result}
+            currenciesDate={currenciesDate}
           />
-        </Label>
-      </p>
-      <p>
-        <Label>
-          <LabelText>Wybierz walutę* :</LabelText>
-          <Input
-            as="select"
-            name="currency"
-            required
-            value={currency}
-            onChange={({ target }) => setCurrency(target.value)}
-          >
-            {currencies.map((currency) => (
-              <option key={currency}>{currency}</option>
-            ))}
-          </Input>
-        </Label>
-      </p>
-      <p>
-        <Button>Przelicz</Button>
-      </p>
-      <Result printResult={printResult} result={result} />
+        </>
+      )}
     </form>
   );
 };
--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -2,31 +2,36 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const useRatesData = () => {
-    const [ratesData, setRatesData] = useState({ status: "loading" });
 
-    useEffect(() => {
-        setTimeout(() => {
-          (async () => {
-            try {
-              // const response = await axios.get("https://api.exchangerate.host/latest?base=PLN")
-              const response = await axios.get("http://127.0.0.1:5500/src/Form/currenciesData.json");
-    
-              localStorage.setItem("data", JSON.stringify(response.data));
-    
-              setRatesData({
-                status: "success",
-              });
+  const [ratesData, setRatesData] = useState({ status: "loading" });
 
-            } catch {
-              setRatesData({
-                status: "error",
-              });
-            }
-          })();
-        }, 3000);
-      }, []);
-    
- 
+  useEffect(() => {
+    setTimeout(() => {
+      (async () => {
+        try {
+          // const response = await axios.get("https://api.exchangerate.host/latest?base=PLN")
+          const response = await axios.get(
+            "http://127.0.0.1:5500/src/Form/currenciesData.json"
+          );
 
-  return ratesData ;
+          localStorage.setItem("data", JSON.stringify(response.data));
+
+          setRatesData({
+            status: "success",
+          });
+        } catch {
+          setRatesData({
+            status: "error",
+          });
+        }
+      })();
+    }, 3000);
+  }, []);
+
+  const dataFromLS = localStorage.getItem("data");
+  const currenciesData = JSON.parse(dataFromLS);
+  const currencies = Object.keys(currenciesData.rates);
+  const currenciesDate = currenciesData.date;
+
+  return {ratesData, currenciesData, currencies, currenciesDate};
 };
